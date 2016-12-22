a36004f4771139814ef9fde38b016fbd27642bf4
Christos Botsas <p13098@students.cs.unipi.gr>
2016-12-23 00:52:33 +0200

    Added Event views and controller.

---
+++ VivaRegistry/Controllers/EventsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VivaRegistry.Models;
+
+namespace VivaRegistry.Controllers
+{
+    public class EventsController : Controller
+    {
+        private VivaRegistryContext db = new VivaRegistryContext();
+
+        
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AppId = new SelectList(db.Applications, "ApplicationId", "ApplicationName", @event.AppId);
+            ViewBag.LogId = new SelectList(db.LogLevels, "LogLevelId", "LogLevelName", @event.LogId);
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EventId,EventGenerationDate,AppId,LogId")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@event).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AppId = new SelectList(db.Applications, "ApplicationId", "ApplicationName", @event.AppId);
+            ViewBag.LogId = new SelectList(db.LogLevels, "LogLevelId", "LogLevelName", @event.LogId);
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event @event = db.Events.Find(id);
+            db.Events.Remove(@event);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- VivaRegistry/Migrations/Configuration.cs
+++ VivaRegistry/Migrations/Configuration.cs
@@ -3,6 +3,7 @@ namespace VivaRegistry.Migrations
     using Models;
     using System;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<VivaRegistryContext>
     {
@@ -15,14 +16,31 @@ namespace VivaRegistry.Migrations
         {
             context.Applications.AddOrUpdate(
                 x => x.ApplicationName,
-                new Application { ApplicationName = "Wallet", CreationDate = DateTime.Now}
+                new Application { ApplicationName = "Wallet", CreationDate = DateTime.Now }
             );
             context.LogLevels.AddOrUpdate(
                 x => x.LogLevelName,
                 new LogLevel { LogLevelName = "Critical" },
                 new LogLevel { LogLevelName = "Error" },
-                new LogLevel { LogLevelName = "Warning"},
-                new LogLevel { LogLevelName = "Information"}
+                new LogLevel { LogLevelName = "Warning" },
+                new LogLevel { LogLevelName = "Information" }
+            );
+
+            context.Events.AddOrUpdate
+            (
+                x => x.EventId,
+                new Event
+                {
+                    AppId = context.Applications.FirstOrDefault(a => a.ApplicationName == "Wallet").ApplicationId,
+                    LogId = context.LogLevels.FirstOrDefault(l => l.LogLevelName == "Error").LogLevelId,
+                    EventGenerationDate = DateTime.Now
+                },
+                new Event
+                {
+                    AppId = context.Applications.FirstOrDefault(a => a.ApplicationName == "Wallet").ApplicationId,
+                    LogId = context.LogLevels.FirstOrDefault(l => l.LogLevelName == "Warning").LogLevelId,
+                    EventGenerationDate = DateTime.Now
+                }
             );
         }
     }
